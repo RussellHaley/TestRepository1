e6481286e967bebe953f816a84ad30a9fdecf43d
russellhaley <russ.haley@gmail.com>
2012-08-26 14:17:14 -0700

    updated my github repository

---
+++ DataAccessLayer/DataAccessLayer/AdoExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OIS.CA.DataAccessLayer
+{
+	public class ADODataAccess
+	{
+		private string UserName { get; set; }
+
+		private string Password { get; set; }
+
+		private string ServerName { get; set; }
+
+		private string DatabaseName { get; set; }
+
+		public string ConString { get; set; }
+
+		public ADODataAccess(string serverName, string userName, string password)
+		{
+			UserName = userName;
+			Password = password;
+			ServerName = serverName;
+			DatabaseName = "PersonModel";
+
+			ConString = "Data source=" + ServerName + "; Initial Catalog=" + DatabaseName + "; user ID=" + UserName + "; password=" + Password;
+			
+		}
+
+		public IDataReader GetPerson(string firstName, string lastName)
+		{
+			SqlConnection con = new SqlConnection(ConString);
+			
+				SqlCommand comm = new SqlCommand();
+				comm.CommandText = "select * from person";
+				comm.Connection = con;
+				con.Open();
+
+				SqlDataReader reader = comm.ExecuteReader(CommandBehavior.CloseConnection);
+
+
+
+				return reader;
+		}
+	}
+}
---
+++ DataAccessLayer/TestClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OIS.CA.DataAccessLayer;
+using System.Data;
+
+
+namespace OIS.CA.TestClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ADODataAccess da = new ADODataAccess("layne\\sqlexpress","DevUser","DevUser");
+
+			Console.Write(da.ConString);
+
+			Console.ReadLine();
+
+			OutputPeople(da.GetPerson("russell", "haley"));
+		}
+
+		static void OutputPeople(IDataReader reader)
+		{
+			while (reader.Read())
+			{
+				Console.WriteLine("\t{0}\t{1}\t{2}\t{3}", reader[0], reader[1], reader[2],reader[3]);
+			}
+			reader.Close();
+			Console.ReadLine();
+		}
+	}
+}
